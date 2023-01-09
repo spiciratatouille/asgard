d6905469889a4168ff5953bef675c997a130c647
Roger Gonzalez <roger@rogs.me>
2023-01-09 20:23:30 -0300

    Replaced newgrp with sudo su $USER

--- docker.sh
+++ docker.sh
@@ -25,4 +25,4 @@ sudo usermod -G docker $USER
 sudo curl -L https://github.com/docker/compose/releases/download/v2.15.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-exec sg docker "newgrp `id -gn`"
+sudo su $USER
