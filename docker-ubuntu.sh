2c2569124139702408591118947af645edb91160
Roger Gonzalez <roger@rogs.me>
2023-01-09 15:50:30 -0300

    Added docker-compose installation

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -17,3 +17,10 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -G docker $USER
+
+exec sg docker "newgrp `id -gn`"
+
+sudo curl -L https://github.com/docker/compose/releases/download/v2.15.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
